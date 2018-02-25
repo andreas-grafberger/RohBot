b3b7dabd0f3c04b3647336957fc6d5fa87757025
Andreas Grafberger <andreasgrafberger@live.de>
2018-02-25 17:58:53 +0100

    Restructured project and added travis file

--- intents/CalculatorIntent.py
+++ intents/CalculatorIntent.py
@@ -1,13 +1,25 @@
 from Intent import Intent
 import re
 
+
 class CalculatorIntent(Intent):
 
     @staticmethod
+    def evaluate(input):
+
+        # First remove whitespaces from string
+        expression = ''.join(input.split())
+
+        result = None
+        match = re.search('([-+]?\d*\.\d+|\d+)([\+\-*/]|\*\*)([-+]?\d*\.\d+|\d+)', expression)
+        if match:
+            result = eval(expression)  # CAUTION: eval can cause security issues. (That is why there is the match before)
+        return result
+
+    @staticmethod
     def execute(string, bot, chat_id):
-        # type: (object, object, object) -> object
-        match = re.search('([-+]?\d*\.\d+|\d+)([\+\-*/]|\*\*)([-+]?\d*\.\d+|\d+)', str(string))
-        if not match:
-            return "I cannot calculate that..."
-        result = eval(string) # CAUTION: eval can cause security issues. (That is why there is the match before)
-        bot.send_message(chat_id, eval(string))
+        answer = CalculatorIntent.evaluate(string)
+        if answer is None:
+            bot.send_message(chat_id, "I cannot calculate that...")
+        else:
+            bot.send_message(chat_id, "Answer: " + answer)
