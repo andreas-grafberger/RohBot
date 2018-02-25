13003542d9d3c6c2bd217ef9ee2b06cef5e1f172
Sarah <sarah-diana.sporck@gmx.de>
2018-02-25 20:23:57 +0100

    extracted creation of message, removed static from internal methods

--- intents/WeatherIntent.py
+++ intents/WeatherIntent.py
@@ -18,23 +18,27 @@ class WeatherIntent(Intent):
                 split.remove(keyword)
         return split # params will be returned
 
-    @staticmethod
-    def getWeatherData(location):
-        data = {'q':location, 'units': 'metric','APPID':loadOWMToken()}
+    def getWeatherData(self, location):
+        appid = loadOWMToken()
+        if appid is None:
+            return None
+        data = {'q':location, 'units': 'metric','APPID':appid}
         r = requests.get(WeatherIntent.weatherUrl, data)
         if r.status_code != 200:
             return None
         return r.json()
+
+    def createMessage(self, data):
+        ioData = {
+            'name': data['name'],
+            'description': data['weather'][0]['description'],
+            'temp': data['main']['temp']
+        }
+        return "In {name} there is a {description} and it has {temp} degree celcius".format(**ioData)
 
     @staticmethod
     def execute(str, bot, chat_id):
         # type: (object, object, object) -> object
         location = WeatherIntent.filterKeyWords(str)[0]
         data = WeatherIntent.getWeatherData(location)
-        ioData = {
-            'name': data['name'],
-            'description': data['weather'][0]['description'],
-            'temp': data['main']['temp']
-        }
-        message = "In {name} there is a {description} and it has {temp} degree celcius".format(**ioData)
-        bot.send_message(chat_id, message)
+        bot.send_message(chat_id, WeatherIntent.createMessage(data))
